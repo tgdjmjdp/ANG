d030aa56e814375c89b80805df123fbc22d6f17a
user00 <tgdjmjdp2@gmail.com>
2019-08-04 23:31:51 +0700

    fetching the posts

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First Middleware');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+      {
+        id : "123",
+        title : "first title",
+        content : "sadaadaadssadssdsad"
+      },
+      {
+        id : "12313",
+        title : "second title",
+        content : "sadaadaadssadssdsad"
+      }
+  ];
+  res.status(200).json({
+    message: "post fetched completely",
+    posts: posts
+  });
 
-  next();
-});
 
-app.use((req, res, next) => {
-  res.send('Hello from express');
 });
 
 module.exports = app;
